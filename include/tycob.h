a91f7796e9132a979b9e54d03cc051db151dfc14
Kenny D. Lee <kennyd.lee@outlook.com>
2016-03-26 11:47:50 -0400

    Create tycob.h

---
+++ include/tycob.h
@@ -0,0 +1,16 @@
+//Mc corn's of the Mc cob, Author...
+ //NAME_LEN 
+ 
+ typedef struct {
+	char MachineNombre [ NAME_LEN + 1 ];
+	double Ver; 
+} Machine;
+ 
+ Machine 
+ linear = { "Pure Digital", 0.4 }, 
+ citro3D = { "citro3D Digital", 9.6 }, 
+ SF2D = { "SF2D Digital", 9.0 }, 
+  
+ ...
+ 
+ Default = { "Lady Zurich" 1.1 };
