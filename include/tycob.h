d197de8272f3f65147116e917f94dfa2fa39ea81
Kenny D. Lee <kennyd.lee@outlook.com>
2016-03-27 02:14:39 -0400

    Update tycob.h

--- include/tycob.h
+++ include/tycob.h
@@ -1,21 +1,28 @@
-//Mc corn's of the Mc cob, Author & your old friend...
+//Mc corn's off the Mc cob, Author & your old friend...
 //
 //  my needs are small, & do find Prairie Grouse's neck chewy ? 
 //
 //please send any hot 19ish years old & over!
 //female specimens U can part with for 3D examination
 
+
+//engines & subsystems list         
+// typedef u8* (*HookLoad)(image imagebuff); typedef not in stone? but it's Must! 
+// typedef (*HookFree)(u8* Region); typedef not in stone? but it's Must! 
+
  //NAME_LEN 
  
  typedef struct {
 	char MachineNombre [ NAME_LEN + 1 ];
 	double Ver; 
+	HookLoad LoadHDC;
+	HookFree FreeHDC; 
 } Machine;
  
  Machine 
- linear = { "Pure Digital", 0.4 }, 
- SF2D = { "SF2D Digital", 9.0 }, 
- citro3D = { "citro3D Digital", 9.6 }, 
+ linear = { "Pure Digital", 0.4, null, null}, 
+ SF2D = { "SF2D Digital", 9.0, null, null}, 
+ citro3D = { "citro3D Digital", 9.6, null, null}, 
   
  ...
  
