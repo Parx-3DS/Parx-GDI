74344082ae3bcc00ab4c69bc81d1b9f8961d73d7
Kenny D. Lee <kennyd.lee@outlook.com>
2016-04-13 20:20:30 -0400

    Update ParxGDI.h
    fixed ordering of Top,Left,Height,Width, --> SetRecEx()

--- include/ParxGDI.h
+++ include/ParxGDI.h
@@ -8,14 +8,18 @@
 
 //24bit bytes_per_pixel(GSP_BGR8_OES) & 240 pixel's high as default in functions SetCanvasPixel & GetCanvasPixel 
 
+//Utils
+void ClrParx(u8* screen, u32 colour);
+u8* RefParx(ParxLCD LCD);
+int MaxWidth(u8* screen);
+
 //Parx Canvas Pixel's
 void SetPixEx(u8* screen, u16 bpp, int h, int x, int y, u32 colour);
-#define SetPix(screen, x, y, colour) (SetPixEx(screen, 3, 240, x, y,colour))
+#define SetPix(screen, x, y, colour) (SetPixEx(screen, 3, SCREEN_Y, x, y,colour))
 u32 GetPixEx(u8* screen, u16 bpp, int h, int x, int y);
-#define GetPix(screen, x, y) (GetPixEx(screen, 3, 240, x, y))
+#define GetPix(screen, x, y) (GetPixEx(screen, 3, SCREEN_Y, x, y))
 
-void ClearParx(u8* screen, u32 colour);
-
+//Parx Canvas Rectangular Region's 
 u32 SetRecEx(u8* screen, int Top, int Left, int Height, int Width, u8* Region, bool CleanUp, RamType RamLocal);
 #define SetRecRe(screen, Top, Left, Height, Width, Region) (SetRecEx(screen, Top, Left, Height, Width, Region, 1, linear))
 
