d6d93e5986035cafbf3193b0eadd050d33204165
Kenny D. Lee <kennyd.lee@outlook.com>
2016-08-01 16:02:57 -0400

    works in FreePascal

---
+++ source/Parx_Debug.c
@@ -0,0 +1,13 @@
+#include "Parx_Debug.h"
+
+void DebugTo(u8* screen, int x, int y, u32 colour, const char* str,...)
+{
+    char* formated[256];
+    va_list ap;
+    va_start(ap, str);
+    snprintf(formated, 256, str, ap);
+    //sprintf(formated, str, ap);
+    va_end(ap);
+    CanvasString(screen, formated, x, y, colour);
+    gfxFlushBuffers();
+}
