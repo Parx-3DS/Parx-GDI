46e4b35795b8582479727b03cb7e05612401dd95
Kenny D. Lee <kennyd.lee@outlook.com>
2017-08-11 02:13:10 -0400

    Delete Parx_Buffer.h

--- include/Parx_Buffer.h
+++
@@ -1,4 +0,0 @@
-#include "ParxGDI.h"
-
-void lineSample(u8* screen, int x0, int y0, int x1, int y1, u8* buffer);
-void EllipseSample(u8* screen, int xctr, int yctr, int majaxis, int minaxis, int rotnrad, int DisplayAxis, u8* buffer);
